87d1b08eef2adbb7c71ae40b04912dabe6a02536
arc lab <turtlebot2@turtlebot2.(none)>
2015-05-28 16:45:47 -0400

    added underscores to 'private' functions

--- src/Move_Manager.py
+++ src/Move_Manager.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+import time
 import math
 # ROS
 import rospy
@@ -7,7 +8,7 @@ from roslib import message
 from geometry_msgs.msg import Twist
 from Direction import *
 class Move_Manager(object):
-	def __init__(self, sensor_manager, min_dist=50, x=1, z=.5, delay=.5, rate=10, hardcode_x = 12):
+	def __init__(self, sensor_manager, min_dist=.75, x=1, z=1, delay=1, rate=10, hardcode_x=12):
 		self.sm = sensor_manager
 		self.tw_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist)
 		self.min_dist = min_dist
@@ -15,9 +16,10 @@ class Move_Manager(object):
 		self.x = x
 		self.hx = hardcode_x
 		self.rate = rospy.Rate(rate)
+		self.delay = delay
 
 	# look right, check distance to wall, return left
-	def check(self, direction):
+	def _check(self, direction):
 		if direction == Direction.RIGHT:
 			self.right()
 			result = self.min_dist < self.sm.read_depth()
@@ -35,19 +37,20 @@ class Move_Manager(object):
 	# we always turn orthogonally so we won't ask for z input
 	def move(self, direction, hardcode=True):
 		if direction == Direction.FORWARD:
-			self.send_twist(self.x, 0)
+			self._send_twist(self.x, 0)
 		elif direction == Direction.BACKWARD:
-			self.send_twist(-self.x, 0)
+			self._send_twist(-self.x, 0)
 		elif direction == Direction.RIGHT or direction == Direction.LEFT:
-			self.turn(direction, hardcode)
+			self._turn(direction, hardcode)
 		else:
 			rospy.loginfo("invalid direction to move()")
+		time.sleep(self.delay)
 
-	def turn(self, direction, hardcode):
+	def _turn(self, direction, hardcode):
 		if hardcode:
-			val = self.hx if direction == Direction.RIGHT else -self.hx
+			val = -self.hx if direction == Direction.RIGHT else self.hx
 			for i in range(self.hx):
-				self.send_twist(0, val)
+				self._send_twist(0, val)
 		else:
 			## Odometry is not as good as I imagined
 			# use odometry sensors
@@ -70,7 +73,7 @@ class Move_Manager(object):
 		twist = Twist()
 		self.tw_pub.publish(twist)
 
-	def send_twist(self, x, z):
+	def _send_twist(self, x, z):
 		twist = Twist()
 		twist.linear.x = x
 		twist.angular.z = z
