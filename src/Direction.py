ff2450026b8a30de6fa11616270357c3ef058d92
arc lab <turtlebot2@turtlebot2.(none)>
2015-05-29 17:51:36 -0400

    comments for clarity

--- src/Direction.py
+++ src/Direction.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python2
 
+'''
+	FORWARD
+LEFT		RIGHT
+	BACKWARD
+'''
+
 class Direction(object):
 	LEFT = 0,
 	RIGHT = 1,
 	FORWARD = 2,
 	BACKWARD = 3,
 	TURN = {
-		(0,):{
+		# LEFT
+		(0,):{ 
+			# LEFT turn --> BACKWARD
 			(0,): (3,),
+			# RIGHT turn --> FORWARD
 			(1,): (2,)
 		},
+		# RIGHT
 		(1,):{
+			# LEFT turn --> FORWARD
 			(0,):(2,),
+			# RIGHT turn --> BACKWARD
 			(1,):(3,)
 		},
+		# FORWARD
 		(2,):{
+			# LEFT turn --> LEFT
 			(0,):(0,),
+			# RIGHT turn --> RIGHT
 			(1,):(1,)
 		},
+		# BACKWARD
 		(3,):{
+			# LEFT turn --> RIGHT
 			(0,):(1,),
+			# RIGHT turn --> LEFT
 			(1,):(0,)
 		}
 	}
