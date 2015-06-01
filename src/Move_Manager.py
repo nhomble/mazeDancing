4adf8427a3f57b38ffb907116991e8e54d0a3e97
arc lab <turtlebot2@turtlebot2.(none)>
2015-06-01 12:55:57 -0400

    more updates and refactoring with consts

--- src/Move_Manager.py
+++ src/Move_Manager.py
@@ -1,63 +1,64 @@
 #!/usr/bin/env python2
+
 import time
 import math
+
 # ROS
 import rospy
 from roslib import message
+
 # movement
 from geometry_msgs.msg import Twist
-from Direction import *
+from consts import *
 
+# NOTE MUST BE import by a ROS NODE
 class Move_Manager(object):
-	def __init__(self, sensor_manager, min_forward_dist=.75, min_turn_dist=.6, x=1, z=1, delay=1, rate=10, hardcode_x=12):
-		# TODO do I need to create another ros node here?
-		self.sm = sensor_manager
-		self.tw_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist)
-
-		# TODO
-		# hardcode distance to wall checks
-		self.min_forward_dist = min_forward_dist
-		self.min_turn_dist = min_turn_dist
-
-		# hardcode movements
-		self.z = z
-		self.x = x
-		self.hx = hardcode_x
+	def __init__(self):
+		self.tw_pub = rospy.Publisher(TWIST_PUB, Twist)
 
 		# delays
-		self.rate = rospy.Rate(rate)
-		self.delay = delay
+		self.rate = rospy.Rate(RATE)
 
+	'''
 	# look right, check distance to wall, return left
 	# return True if should/can move there
 	# this is not in Sensor since we have to move the bot to do the checks
 	def check(self, direction):
+		result = None
+		dist = None
 		if direction == Direction.RIGHT:
 			self.move(Direction.RIGHT)
-			result = self.min_turn_dist < self.sm.bias_pcl(direction)
+			while dist is None:
+				dist = self.sm.bias_pcl(direction)
+			result = self.min_turn_dist < dist
 			self.move(Direction.LEFT)
 		elif direction == Direction.LEFT:
 			self.move(Direction.LEFT)
-			result = self.min_turn_dist < self.sm.bias_pcl(direction)
+			while dist is None:
+				dist = self.sm.bias_pcl(direction)
+			result = self.min_turn_dist < dist
 			self.move(Direction.RIGHT)
 		elif direction == Direction.FORWARD:
-			result = self.min_forward_dist < self.sm.bias_pcl(direction)
+			while dist is None:
+				dist = self.sm.bias_pcl(direction)
+			result = self.min_forward_dist < dist
 		else:
 			rospy.log("you asked to check backwards?")
 		return result
+	'''
 	
 	# we always turn orthogonally so we won't ask for z input
 	def move(self, direction, hardcode=True):
 		# NOTE this should be the only place where we delay after movement
-		time.sleep(self.delay)
 		if direction == Direction.FORWARD:
-			self._send_twist(self.x, 0)
+			self._send_twist(-TWIST_X, 0)
 		elif direction == Direction.BACKWARD:
-			self._send_twist(-self.x, 0)
+			self._send_twist(TWIST_X, 0)
 		elif direction == Direction.RIGHT or direction == Direction.LEFT:
 			self._turn(direction, hardcode)
 		else:
-			rospy.loginfo("invalid direction to move()")
+			rospy.loginfo("invalid direction to move() " + str(direction))
+		time.sleep(2)
 
 	# halt movement of the turtlebot immediately
 	def stop(self):
@@ -67,11 +68,12 @@ class Move_Manager(object):
 	def _turn(self, direction, hardcode):
 		# HACK we just hardcode a fixed number of identical twist messages to do
 		# an orthogonal turn on a flat surface
+		val = TWIST_Z if direction == Direction.RIGHT else -TWIST_Z
 		if hardcode:
-			val = -self.hx if direction == Direction.RIGHT else self.hx
-			for i in range(self.hx):
+			for i in range(TWIST_TURN_NUM):
 				self._send_twist(0, val)
 		else:
+			return # code is broken below
 			# NOTE Odometry is not as good as I imagined
 			_, curr_angle = self.sm.curr_angle()
 			if curr_angle is None:
