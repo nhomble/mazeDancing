6e0a7623fbda02e22f3d97c6ad86f7785cd548a0
arc lab <turtlebot2@turtlebot2.(none)>
2015-06-01 11:27:13 -0400

    consolidating directions in consts.py

--- src/consts.py
+++ src/consts.py
@@ -23,3 +23,45 @@ DEPTH_SUB = '/camera/depth/image'
 COLOR_SUB = '/camera/rgb/image_color'
 
 TWIST_PUB = 'cmd_vel_mux/input/teleop'
+
+'''
+	FORWARD
+LEFT		RIGHT
+	BACKWARD
+'''
+
+class Direction(object):
+	LEFT = 0,
+	RIGHT = 1,
+	FORWARD = 2,
+	BACKWARD = 3,
+	TURN = {
+		# LEFT
+		(0,):{ 
+			# LEFT turn --> BACKWARD
+			(0,): (3,),
+			# RIGHT turn --> FORWARD
+			(1,): (2,)
+		},
+		# RIGHT
+		(1,):{
+			# LEFT turn --> FORWARD
+			(0,):(2,),
+			# RIGHT turn --> BACKWARD
+			(1,):(3,)
+		},
+		# FORWARD
+		(2,):{
+			# LEFT turn --> LEFT
+			(0,):(0,),
+			# RIGHT turn --> RIGHT
+			(1,):(1,)
+		},
+		# BACKWARD
+		(3,):{
+			# LEFT turn --> RIGHT
+			(0,):(1,),
+			# RIGHT turn --> LEFT
+			(1,):(0,)
+		}
+	}
