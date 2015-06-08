733b800222a72309bfecfb84906897cfef90c24f
arc lab <turtlebot2@turtlebot2.(none)>
2015-06-08 15:04:05 -0400

    fixed the max/min constants for the x range (the interval was too small)

--- src/consts.py
+++ src/consts.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python2
 
 MIN_FORWARD_DIST = .75	# no closer to the wall than this empirically chosen value
-MAX_FORWARD_DIST = 1.2	# no further away to the wall than this empirically chosen value
+MAX_FORWARD_DIST = 0.9	# no further away to the wall than this empirically chosen value
 MIN_TURN_DIST = .6		# ^ ditto but for turning
-MAX_TURN_DIST = 1.0
+MAX_TURN_DIST = .8
 
 # taken from ROS mini_max tutorial
-TWIST_X = .15		# .15 m/s
-TWIST_Z = 1.57/2	# 45 deg/s * 2 sec = 90 degress
 TWIST_NUM = 10		
-RATE = 5.0			# 10 * 5 Hz = 2 sec
-DELAY = 1			# hard 1 second delay
+RATE = 5.0
+TIME = TWIST_NUM / RATE
+TWIST_X = .15			# .15 m/s
+TWIST_Z = 1.57/TIME		# 45 deg/s * 2 sec = 90 degress
+DELAY = 1				# hard 1 second delay
 
 SPEECH_NODE = 'espeak_node'
 SPEECH_IO = 'espeak_sub'
@@ -21,9 +22,9 @@ PCL_RIGHT_IO = 'pcl/right'
 PCL_LEFT_IO = 'pcl/left'
 PCL_MIDDLE_IO = 'pcl/middle'
 
-PCL_X_MIN = -0.35
+PCL_X_MIN = -1.0
 PCL_Y_MIN = 0.35
-PCL_X_MAX = 0.1
+PCL_X_MAX = 1.0
 PCL_Y_MAX = 1.2
 
 ODOM_SUB = 'odom'
