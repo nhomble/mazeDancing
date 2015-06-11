8ca295b30e98922d61884105800a8e6cfb06c3f6
arc lab <turtlebot2@turtlebot2.(none)>
2015-06-11 13:43:29 -0400

    given a list of turns, I will translate that into a list of directions

--- src/Dance.py
+++ src/Dance.py
@@ -3,30 +3,28 @@
 from consts import *
 import time
 
-# execute movements
-def do_circle(d, move):
-	for _ in range(4):
-		move.move(d)
-
 # for each movement:
 #	move in that direction for a whole circle
 #	then pause  to delimit the directions
-def do_dance(directions, move, follower=Follower.RIGHT):
+def do_dance(directions, move):
 	for d in directions:
-		if d == Direction.LEFT:
-			if follower is not Follower.LEFT:
-				raise Exception("I cannot move left with right follower")
-		elif d == Direction.RIGHT:
-			if follower is not Follower.RIGHT:
-				raise Exception("I cannot move right with left follower")
-		elif d == Direction.FORWARD:
-			d = TRANSLATE_FORWARD[follower]
-		_do_circle(d, move)
-		move.stop()
-		time.sleep(1)
+		clock = Language.DIR_TO_CLOCK[d]
+		move.move(clock[0])
+		time.sleep(2)
+		move.move(clock[1])
+
+def get_directions(turns):
+	i = 2
+	directions = []
+	while i <= len(turns):
+		couple = turns[i-2:i]
+		directions.append(Language.CLOCK_TO_DIR[couple[0]][couple[1]])
+		i += 2
+	return directions
 
 # TODO
 # need to do image processing to detect movements
 def interpret_dance():
 	directions = []
 	return directions
+
