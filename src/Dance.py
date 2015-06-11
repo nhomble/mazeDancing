f3046c0303e5339c3605f67b50d9f65ef7e37267
arc lab <turtlebot2@turtlebot2.(none)>
2015-06-11 15:19:31 -0400

    first implementation of dance interpreter done

--- src/Dance.py
+++ src/Dance.py
@@ -2,6 +2,12 @@
 
 from consts import *
 import time
+import rospy
+from ar_track_alvar.msg import AlvarMarkers
+
+'''
+SCOUT
+'''
 
 # for each movement:
 #	move in that direction for a whole circle
@@ -13,18 +19,81 @@ def do_dance(directions, move):
 		time.sleep(2)
 		move.move(clock[1])
 
-def get_directions(turns):
-	i = 2
-	directions = []
-	while i <= len(turns):
-		couple = turns[i-2:i]
-		directions.append(Language.CLOCK_TO_DIR[couple[0]][couple[1]])
-		i += 2
-	return directions
+'''
+=======================================================
+'''
 
-# TODO
+'''
+WORKER
+'''
+
+_detected_tags = []
+_last_tag = None
+_is_done = False
+_dirs = []
+_turns = []
+
+def _translate(l, trans):
+	i = 2
+	ret = []
+	while i <= len(l):
+		couple = l[i-2:i]
+		rospy.loginfo(couple)
+		ret.append(trans[couple[0]][couple[1]])
+		i += 2
+	return ret
+
+def _get_turns(tags):
+	return _translate(tags, Language.ID_TO_CLOCK)
+def _get_directions(turns):
+	return _translate(turns, Language.CLOCK_TO_DIR)
+
+def _check_is_done(tags):
+	global _dirs
+	global _turns
+	_turns = _get_turns(tags)
+	_dirs = _get_directions(turns)
+	return True if _dirs[-1] is None else False
+
+def _tag_callback(data):
+	global _detected_tags
+	global _last_tag
+	global _is_done
+	global _dirs
+	if len(data.markers) == 0:
+		return
+	_marker  = data.markers[0]
+	if _marker.id < 3 or _marker.id > 7:
+		return
+	tag = Tag.translate_id(_marker.id)
+	if _is_done:
+		return
+	# init
+	if _last_tag is None:
+		_last_tag = tag
+		_detected_tags.append(tag)
+	# we got a new position
+	elif _last_tag != tag:
+		_last_tag = tag
+		_detected_tags.append(tag)
+
+		# check end condition
+		_is_done = False if len(_detected_tags) < 4 or len(_detected_tags) % 4 != 0 else _check_is_done(_detected_tags)
+	
+	if _is_done:
+		del _dirs[-1]
+
 # need to do image processing to detect movements
 def interpret_dance():
-	directions = []
-	return directions
+	_detected_turns = []
+	sub = rospy.Subscriber('/ar_pose_marker', AlvarMarkers, _tag_callback)
+	while not _is_done and not rospy.is_shutdown():
+		print(_detected_tags)
+		print(_turns)
+		print(_dirs)
+		rospy.Rate(DELAY).sleep()
+	return _dirs
 
+'''
+========================================================
+'''
