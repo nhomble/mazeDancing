69057d1d1bcffb4ddac8790a0b65947c02ca9211
tb04-l <turtlebot@tb04-l.(none)>
2015-06-17 16:17:20 -0400

    tweaking

--- src/Dance.py
+++ src/Dance.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import math
 import roslib; roslib.load_manifest('mazeDancing')
 import rospy
 from consts import *
@@ -38,9 +39,8 @@ _turns = []
 def _translate(l, trans):
 	i = 2
 	ret = []
-	while i <= len(l):
-		couple = l[i-2:i]
-		rospy.loginfo(couple)
+	while i < len(l) - 1:
+		couple = (l[i], l[i+1])
 		ret.append(trans[couple[0]][couple[1]])
 		i += 2
 	return ret
@@ -76,20 +76,26 @@ def _tag_callback(data):
 		_detected_tags.append(tag)
 	# we got a new position
 	elif _last_tag != tag:
+		# sometimes we skip
+#		if abs(tag[0] - _last_tag[0]) == 2:
+#			_detected_tags.append(Tag.translate_id((tag[0] + _last_tag[0]) / 2))
 		_last_tag = tag
 		_detected_tags.append(tag)
 
 		# check end condition
-		_is_done = False if len(_detected_tags) < 4 or len(_detected_tags) % 4 != 0 else _check_is_done(_detected_tags)
+		_is_done = False if len(_detected_tags) < 4 or len(_detected_tags) % 2 != 1 else _check_is_done(_detected_tags)
 	
-	if _is_done:
-		del _dirs[-1]
+#	if _is_done:
+#		del _dirs[-1]
 
 # need to do image processing to detect movements
 def interpret_dance():
 	_detected_turns = []
 	sub = rospy.Subscriber('/ar_pose_marker', AlvarMarkers, _tag_callback)
 	while not _is_done and not rospy.is_shutdown():
+		rospy.loginfo(_detected_tags)
+		rospy.loginfo(_turns)
+		rospy.loginfo(_dirs)
 		rospy.Rate(DELAY).sleep()
 	sub.unregister()
 	return _dirs
